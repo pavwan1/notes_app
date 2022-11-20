c8be2134999479bc1ebcf5e4ae002fd96e4a5f47
pavwan <pavanreddy20021@gmail.com>
2022-11-21 00:47:56 +0530

    Title and it's logo is Updated

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,7 +1,7 @@
 import {MdDeleteForever} from "react-icons/md";
 const Note = ({id, text, date, handleDeleteNote}) => {
     return(
-        <div className="note">
+        <div className="note" >
             <span>{text}</span>
             <div className="note-footer">
                 <small>{date}</small>
